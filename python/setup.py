fa305b34cbb7f4a895946b759f24c7301b668ef4
Kevin Murray <k.d.murray.91@gmail.com>
2013-12-16 12:39:10 +1100

    add requirements to setup.py

--- python/setup.py
+++ python/setup.py
@@ -5,10 +5,27 @@ traitcapture-bin:
     A series of scripts that do various stages of the traitcapture pipeline.
 """
 
+install_requires = [
+        "ExifRead==1.4.2",
+        "Pillow>=2.2.0",
+        "docopt==0.6.1",
+        "filemagic==1.6",
+        "imageio==0.4.1",
+        ]
+
+test_requires = [
+        "coverage==3.7.1",
+        "nose==1.3.0",
+        "pep8==1.4.6",
+        "pylint==1.0.0",
+        ]
+
 setup(
     name="traitcapture-bin",
     py_modules=['exif2timestream', ],
     version="0.1a",
+    install_requires=install_requires,
+    tests_require=test_requires,
     description=desc,
     author="Kevin Murray",
     author_email="spam@kdmurray.id.au",
