736a6e52af8b644140cd6df41c66bb914bc2fa7f
Kevin Murray <spam@kdmurray.id.au>
2014-01-30 18:20:24 +1100

    add 0.1b1 version of hafresize.sh

--- hafresize/hafresize.sh
+++ hafresize/hafresize.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 
+if [ $# -ne 3 ]
+then
+	echo "HAFResize.sh -- The hacky-as-fuck timestream resizer"
+	echo
+	echo "USAGE:"
+	echo "	hafresize.sh <timestream> <x> <y>"
+	echo "OPTIONS:"
+	echo "	<timestream>	Path to timestream to be resized."
+	echo "	<x>		X coordinate to resize to."
+	echo "	<y>		Y coordinate to resize to. If -1, it is calculated from the first image's aspect ratio."
+	exit
+fi
+
 ts="${1}"
-newres="${2}x${3}"
-echo "resizing $ts to $newres"
+
+if [ $3 -gt 0 ]
+then
+	newres="${2}x${3}"
+else
+	newres="$2"
+fi
+
+if [ ! -d $ts ]
+then
+	echo "ERROR: timestream must exist" >&2
+	exit -1
+fi
 
 new_ts=$(echo $ts |  sed -e "s/fullres/$newres/g")
 if [ -d "$new_ts" ]
 then
-	echo "new timestream already exists"
-	exit
+	echo "ERROR: new timestream already exists" >&2
+	exit -1
 fi
 
+echo "resizing $ts to $newres"
 dirs=$(find $ts -type d -print | sed -e "s/fullres/$newres/g")
 echo "$dirs" | parallel mkdir -p
 
