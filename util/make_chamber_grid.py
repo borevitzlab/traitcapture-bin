fd74615b3bb88a7ab71bf00be6a8de639f18cd94
Kevin Murray <spam@kdmurray.id.au>
2014-04-01 18:48:29 +1100

    modularise make_chamber_grid.py to enable tests

--- util/make_chamber_grid.py
+++ util/make_chamber_grid.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
-chamber_pos = []
+def make_chamber_pos():
+    chamber_pos = []
 
-for tray in xrange(1,17):
-    for t_col in ['A', 'B', 'C', 'D']:
-        for t_row in xrange(1, 6):
-            chamber_pos.append("%d%s%d" % (tray, t_col, t_row))
+    for tray in xrange(1,17):
+        for t_col in ['A', 'B', 'C', 'D']:
+            for t_row in xrange(1, 6):
+                chamber_pos.append("%d%s%d" % (tray, t_col, t_row))
 
-for iii, pos in enumerate(chamber_pos):
-    print "%i\t%s" % (iii + 1, pos)
+    for iii, pos in enumerate(chamber_pos):
+        yield "%i\t%s" % (iii + 1, pos)
+
+if __name__ == "__main__":
+    for s in make_chamber_pos():
+        print s
